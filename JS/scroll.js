f4b0776f18cad95d93ddcbad4625b7369b1f041f
kevin raja <rajananthan.kevin@protonmail.ch>
2023-09-16 01:53:23 +0200

    portfolio

---
+++ JS/scroll.js
@@ -0,0 +1,30 @@
+const sr = ScrollReveal();
+        sr.reveal('.logo',{
+            origin: 'left',
+            distance:'100px',
+            duration: 1500,
+            reset:true,
+            
+        });
+        sr.reveal('.textBox',{
+            origin: 'left',
+            distance:'100px',
+            duration: 1500,
+            reset:true,
+            delay : 400
+        });
+        sr.reveal('.text1',{
+            origin: 'left',
+            distance:'100px',
+            duration: 1500,
+            reset:true,
+            
+            
+        });
+        sr.reveal('.container5',{
+            origin: 'left',
+            distance:'100px',
+            duration: 2000,
+            reset:true,
+            
+        });
